54dcdcd1933cd02480169fb019a7522d46ec54e8
Maksim Filippov <maksimphilippov@yahoo.com>
2023-06-23 11:56:46 +0400

    labels and frequecy debug

--- src/components/MoneyActionComponent.tsx
+++ src/components/MoneyActionComponent.tsx
@@ -38,7 +38,11 @@ export default function MoneyActionComponent(prop: MoneyActionComponentProp) {
   }
 
   function validateFrequency(newFrequency: string) {
-    setLocalAction({ ...localAction, frequency: parseInt(newFrequency) });
+    let value = parseInt(newFrequency);
+    if (Number.isNaN(value)) {
+      value = 0;
+    }
+    setLocalAction({ ...localAction, frequency: value });
   }
 
   function validateIncomeValue(newIncomeValue: string) {
@@ -52,69 +56,103 @@ export default function MoneyActionComponent(prop: MoneyActionComponentProp) {
 
   return (
     <div className={className}>
-      {editMode ? (
-        <button className="regular-button" onClick={save}>
-          save
-        </button>
-      ) : (
-        <button className="regular-button" onClick={() => setEditMode(true)}>
-          edit
-        </button>
-      )}
-      <SwitchableInput
-        value={localAction.name}
-        enabled={editMode}
-        validationFunction={validateName}
-      />
-      <SwitchableInput
-        value={String(
-          localAction.isActive
-            ? localAction.investment
-            : -localAction.investment
+      <div>
+        {editMode ? (
+          <button className="regular-button" onClick={save}>
+            save
+          </button>
+        ) : (
+          <button className="regular-button" onClick={() => setEditMode(true)}>
+            edit
+          </button>
         )}
-        enabled={editMode}
-        validationFunction={validateInvestment}
-      />
-      {/* <SwitchableInput
+      </div>
+
+      <p>
+        Title:
+        <SwitchableInput
+          value={localAction.name}
+          enabled={editMode}
+          validationFunction={validateName}
+        />
+      </p>
+      <p>
+        Investment:
+        <SwitchableInput
+          value={String(
+            localAction.isActive
+              ? localAction.investment
+              : -localAction.investment
+          )}
+          enabled={editMode}
+          validationFunction={validateInvestment}
+        />
+      </p>
+      <p>
+        Begining date:
+        {/* <SwitchableInput
         value={localAction.name}
         enabled={editMode}
         validationFunction={validateName}
       /> */}
-      <SwitchableDropdown
-        enabled={editMode}
-        currentValue={localAction.regularity}
-        setChose={(option) =>
-          setLocalAction({ ...localAction, regularity: option })
-        }
-      />
-      <SwitchableInput
-        value={String(localAction.frequency)}
-        enabled={editMode}
-        validationFunction={validateFrequency}
-      />
-      <SwitchableCheckbox
-        value={localAction.isPercentageIncome}
-        setter={(value) =>
-          setLocalAction({ ...localAction, isPercentageIncome: value })
-        }
-        accessedToConfig={editMode}
-      />
-      <SwitchableCheckbox
-        value={localAction.isIncomeIncrementsInvenstment}
-        setter={(value) =>
-          setLocalAction({
-            ...localAction,
-            isIncomeIncrementsInvenstment: value,
-          })
-        }
-        accessedToConfig={editMode}
-      />
-
-      <SwitchableInput
-        value={String(localAction.IncomeValue)}
-        enabled={editMode}
-        validationFunction={validateIncomeValue}
-      />
+      </p>
+      <p>
+        Regularity:
+        <SwitchableDropdown
+          enabled={editMode}
+          currentValue={localAction.regularity}
+          setChose={(option) =>
+            setLocalAction({ ...localAction, regularity: option })
+          }
+        />
+      </p>
+      <p>
+        Frequency:
+        <SwitchableInput
+          value={String(localAction.frequency)}
+          enabled={editMode}
+          validationFunction={validateFrequency}
+        />
+      </p>
+      <p>
+        <SwitchableCheckbox
+          value={localAction.isPercentageIncome}
+          setter={(value) =>
+            setLocalAction({ ...localAction, isPercentageIncome: value })
+          }
+          accessedToConfig={editMode}
+        />
+        {localAction.isActive ? (
+          <span>Income is percentage of investment</span>
+        ) : (
+          <span>Spending is percentage of debt</span>
+        )}
+      </p>
+      <p>
+        <SwitchableCheckbox
+          value={localAction.isIncomeIncrementsInvenstment}
+          setter={(value) =>
+            setLocalAction({
+              ...localAction,
+              isIncomeIncrementsInvenstment: value,
+            })
+          }
+          accessedToConfig={editMode}
+        />
+        {localAction.isActive ? (
+          <span>Income automaticaly added to investments</span>
+        ) : (
+          <span>Spending automaticaly added to debt</span>
+        )}
+      </p>
+      <p>
+        {localAction.isActive ? <span>Income: </span> : <span>Spending: </span>}
+        <SwitchableInput
+          value={String(localAction.IncomeValue)}
+          enabled={editMode}
+          validationFunction={validateIncomeValue}
+        />
+      </p>
     </div>
   );
 }
